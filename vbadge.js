89bf28abd3156aec9f1d015ed7be43d877248bf2
Kerruba <luca.cherubin@gmail.com>
2015-10-02 14:00:00 +0100

    First Commit

---
+++ vbadge.js
@@ -0,0 +1,29 @@
+(function(){
+	"use strict";
+
+	Vue.component('badge', {
+		template: "<img :src='url' />",
+		
+		props: {
+			key: String,
+			value: String,
+			color: String,
+			style: {
+				type: String,
+				default: "flat",
+				// validator: function(value) {
+				// 	var validValues = ["flat","plastic","flat-square","social"];
+				// 	return (validValues.indexOf(value) > -1);
+				// }
+			}
+		},
+		computed: {
+			url: function() {
+				var url = "https://img.shields.io/badge/" + this.key + "-" + this.value + "-" + this.color + ".svg?style=" + this.style; 
+				console.log(url);
+				return url;
+			},
+		}
+	});
+})();
+
